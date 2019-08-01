d5e725e5ac8c84baca43cd268e3222e94395ad78
Jens <jens@totemic.com>
2019-08-01 11:00:16 -0700

    Replace installation of udev with requiring separate package for it

--- conanfile.py
+++ conanfile.py
@@ -14,11 +14,10 @@ class LibUSBConan(ConanFile):
     version = "1.0.22"
     settings = "os", "compiler", "build_type", "arch"
     topics = ("conan", "libusb", "usb", "device")
-    options = {"shared": [True, False], "enable_udev": [True, False], "fPIC": [True, False]}
-    default_options = {'shared': False, 'enable_udev': True, 'fPIC': True}
+    options = {"shared": [True, False], "enableUdev": [True, False], "fPIC": [True, False]}
+    default_options = {'shared': True, 'enableUdev': True, 'fPIC': True}
     homepage = "https://github.com/libusb/libusb"
-    url = "http://github.com/bincrafters/conan-libusb"
-    author = "Bincrafters <bincrafters@gmail.com>"
+    url = "http://github.com/jens-totemic/conan-libusb"
     license = "LGPL-2.1"
     description = "A cross-platform library to access USB devices"
     _source_subfolder = "source_subfolder"
@@ -35,40 +34,44 @@ class LibUSBConan(ConanFile):
 
     def config_options(self):
         if self.settings.os != "Linux":
-            del self.options.enable_udev
+            del self.options.enableUdev
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             del self.options.fPIC
 
-    def system_requirements(self):
-        if self.settings.os == "Linux":
-            if self.options.enable_udev:
-                package_tool = tools.SystemPackageTool()
-                libudev_name = ""
-                os_info = tools.OSInfo()
-                if os_info.with_apt:
-                    libudev_name = "libudev-dev"
-                    if tools.detected_architecture() == "x86_64" and str(self.settings.arch) == "x86":
-                        libudev_name += ":i386"
-                    elif "x86" in tools.detected_architecture():
-                        if str(self.settings.arch) == "armhf":
-                            libudev_name += ":armhf"
-                        elif str(self.settings.arch) == "arm64":
-                            libudev_name += ":arm64"
-                elif os_info.with_yum:
-                    libudev_name = "libudev-devel"
-                    if tools.detected_architecture() == "x86_64" and str(self.settings.arch) == "x86":
-                        libudev_name += ".i686"
-                elif os_info.with_zypper:
-                    libudev_name = "libudev-devel"
-                    if tools.detected_architecture() == "x86_64" and str(self.settings.arch) == "x86":
-                        libudev_name = "libudev-devel-32bit"
-                elif os_info.with_pacman:
-                    libudev_name = "libsystemd systemd"
-                else:
-                    self.output.warn("Could not install libudev: Undefined package name for current platform.")
-                    return
-                package_tool.install(packages=libudev_name, update=True)
+    # def system_requirements(self):
+    #     if self.settings.os == "Linux":
+    #         if self.options.enableUdev:
+    #             package_tool = tools.SystemPackageTool()
+    #             libudev_name = ""
+    #             os_info = tools.OSInfo()
+    #             if os_info.with_apt:
+    #                 libudev_name = "libudev-dev"
+    #                 if tools.detected_architecture() == "x86_64" and str(self.settings.arch) == "x86":
+    #                     libudev_name += ":i386"
+    #                 elif "x86" in tools.detected_architecture():
+    #                     if str(self.settings.arch) == "armhf":
+    #                         libudev_name += ":armhf"
+    #                     elif str(self.settings.arch) == "arm64":
+    #                         libudev_name += ":arm64"
+    #             elif os_info.with_yum:
+    #                 libudev_name = "libudev-devel"
+    #                 if tools.detected_architecture() == "x86_64" and str(self.settings.arch) == "x86":
+    #                     libudev_name += ".i686"
+    #             elif os_info.with_zypper:
+    #                 libudev_name = "libudev-devel"
+    #                 if tools.detected_architecture() == "x86_64" and str(self.settings.arch) == "x86":
+    #                     libudev_name = "libudev-devel-32bit"
+    #             elif os_info.with_pacman:
+    #                 libudev_name = "libsystemd systemd"
+    #             else:
+    #                 self.output.warn("Could not install libudev: Undefined package name for current platform.")
+    #                 return
+    #             package_tool.install(packages=libudev_name, update=True)
 
+    def requirements(self):
+        if self.settings.os == "Linux":
+            self.requires("libudev1/237@totemic/stable")
+            
     def _build_visual_studio(self):
         with tools.chdir(self._source_subfolder):
             solution_file = "libusb_2015.sln"
@@ -103,7 +106,7 @@ class LibUSBConan(ConanFile):
         configure_args = ['--enable-shared' if self.options.shared else '--disable-shared']
         configure_args.append('--enable-static' if not self.options.shared else '--disable-static')
         if self.settings.os == "Linux":
-            configure_args.append('--enable-udev' if self.options.enable_udev else '--disable-udev')
+            configure_args.append('--enable-udev' if self.options.enableUdev else '--disable-udev')
         self._build_autotools(configure_args)
 
     def build(self):
@@ -137,8 +140,8 @@ class LibUSBConan(ConanFile):
         self.cpp_info.includedirs.append(os.path.join("include", "libusb-1.0"))
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
-            if self.options.enable_udev:
-                self.cpp_info.libs.append("udev")
+            # if self.options.enableUdev:
+            #     self.cpp_info.libs.append("udev")
         elif self.settings.os == "Macos":
             self.cpp_info.libs.append("objc")
             self.cpp_info.libs.append("-Wl,-framework,IOKit")
